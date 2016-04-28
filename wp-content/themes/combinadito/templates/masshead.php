ce16ebc335009fc89e8a96991eabd7dda28ea508
root <root@localhost.localdomain>
2016-04-27 20:55:27 -0500

    Masshead Principal

--- wp-content/themes/combinadito/templates/masshead.php
+++ wp-content/themes/combinadito/templates/masshead.php
@@ -1,6 +1,6 @@
 <?php if(is_front_page()){ ?>
 	<section class="masshead home">
-	
+	<img src="<?php echo get_template_directory_uri(); ?>/dist/images/masshead--home.png" class="img-responsive" width="50%" />
 <?php }else {?>
 	<section class="masshead">
 <?php }?>
